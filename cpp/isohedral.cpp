7a471644c07d54148caac0a7eea525788acdf06f
Michael Yang <michael777yang@gmail.com>
2025-01-28 17:34:41 -0500

    Implement half turn tiling checker.

--- cpp/isohedral.cpp
+++ cpp/isohedral.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 #include <iostream>
 #include <set>
 #include <string>
@@ -11,11 +12,24 @@
 
 using namespace std;
 
+using Factor = pair<int, int>;
+
+void printFactor(const Factor& f) {
+  cout << f.first << " " << f.second << "\n";
+}
+
 unordered_map<char, char> COMPLEMENT = {
   {'N', 'S'},
   {'S', 'N'},
   {'E', 'W'},
   {'W', 'E'}
+};
+
+unordered_map<char, char> CCW = {
+  {'N', 'W'},
+  {'E', 'N'},
+  {'S', 'E'},
+  {'W', 'S'}
 };
 
 int longest_match(const string& S1, const string& S2, size_t ub) {
@@ -24,6 +38,23 @@ int longest_match(const string& S1, const string& S2, size_t ub) {
     ++i;
   }
   return i;
+}
+
+vector<Factor> is_double_palindrome(const Factor& F, const vector<vector<Factor>>& palindrome_factor_starts, const vector<vector<Factor>>& palindrome_factor_ends, int n) {
+  int F_len = F.second - F.first + 1 + n * (F.second < F.first);
+  for (const auto& F1: palindrome_factor_starts[F.first]) {
+    int F1_len = F1.second - F1.first + 1 + n * (F1.second < F1.first);
+    if (F1_len == F_len) {
+      return {F1};
+    }
+    for (const auto& F2: palindrome_factor_ends[F.second]) {
+      int F2_len = F2.second - F2.first + 1 + n * (F2.second < F2.first);
+      if (F_len == F1_len + F2_len) {
+        return {F1, F2};
+      }
+    }
+  }
+  return {};
 }
 
 string inv_comp(const std::string& S) {
@@ -36,9 +67,9 @@ string inv_comp(const std::string& S) {
   return result;
 }
 
-vector<pair<int, int>> admissible_mirror_factors(const std::string& P) {
+vector<Factor> admissible_mirror_factors(const std::string& P) {
   int n = P.length();
-  vector<pair<int, int>> factors;
+  vector<Factor> factors;
 
   // Compute admissible mirror factors starting between letter pairs
   for (int i = 0; i < n; ++i) {
@@ -66,11 +97,70 @@ vector<pair<int, int>> admissible_mirror_factors(const std::string& P) {
   return factors;
 }
 
-vector<pair<int, int>> has_translation_tiling(const string& P) {
+vector<pair<Factor, Factor>> admissible_gapped_mirror_factor_pairs(const string& P) {
   int n = P.length();
-  vector<pair<int, int>> factors = admissible_mirror_factors(P);
-  vector<set<pair<int, int>>> factor_starts(n);
-  vector<set<pair<int, int>>> factor_ends(n);
+  vector<pair<Factor, Factor>> factor_pairs;
+  // Compute admissible mirror factors starting between letter pairs
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      int l = longest_match(inv_comp(P + P.substr(0, i)), P.substr(j) + P, (i + n - j) / 2);
+      int r = longest_match(P.substr(i) + P, inv_comp(P + P.substr(0, j)), (j - i) / 2);
+
+      if (l == r && r > 0) {
+        factor_pairs.push_back({make_pair((i-l+n)%n, (i-1+r+n)%n), make_pair((j-l+n)%n, (j-1+r+n)%n)});
+      }
+    }
+  }
+
+  // Compute admissible mirror factors starting in the middle of a letter
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      if (P[i] == COMPLEMENT[P[j]]) {
+        int l = longest_match(inv_comp(P + P.substr(0, i)), P.substr((j + 1) % n), (i + n - j - 1)/2);
+        int r = longest_match(P.substr((i+1)%n) + P, inv_comp(P + P.substr(0, j)), (j-i-1)/2);
+        if (l == r) {
+          factor_pairs.push_back({make_pair((i-l+n)%n, (i+r)%n), make_pair((j-l+n)%n, (j+r)%n)});
+        }
+      }
+    }
+  }
+  return factor_pairs;
+}
+
+vector<Factor> admissible_rotadrome_factors(const string& P, int theta) {
+  int n = P.length();
+  vector<Factor> factors;
+  // Compute admissible palindrome factors starting between letter pairs
+  for (int i = 0; i < n; ++i) {
+    string firstString = P + P.substr(0, i);
+    reverse(firstString.begin(), firstString.end());
+    string rotString(n + (n - i), 'X');
+    string secondString = P.substr(i) + P;
+    for (int i = 0; i < secondString.length(); ++i) {
+      rotString[i] = (theta == 90) ? CCW[secondString[i]] : secondString[i];
+    }
+    int l = longest_match(firstString, rotString, n/2);
+    if (l > 0) {
+      factors.push_back(make_pair((i-l+n)%n, (i-1+l)%n));
+    }
+  }
+  // Compute admissible palindrome factors starting in middle of a letter
+  if (theta == 180) {
+    for (int i = 0; i < n; ++i) {
+      string firstString = P + P.substr(0, i);
+      reverse(firstString.begin(), firstString.end());
+      int l = longest_match(firstString, P.substr((i+1)%n) + P, n/2);
+      factors.push_back(make_pair((i-l+n)%n, (i+l)%n));
+    }
+  }
+  return factors;
+}
+
+vector<Factor> has_translation_tiling(const string& P) {
+  int n = P.length();
+  vector<Factor> factors = admissible_mirror_factors(P);
+  vector<set<Factor>> factor_starts(n);
+  vector<set<Factor>> factor_ends(n);
   for (auto& f: factors) {
     factor_starts[f.first].insert(f);
     factor_ends[f.second].insert(f);
@@ -86,10 +176,72 @@ vector<pair<int, int>> has_translation_tiling(const string& P) {
         if (AB_len == n / 2) {
           return {A, B};
         }
-        pair<int, int> C = {(B.second + 1)%n, (A.first + n/2 - 1 + n)%n};
+        Factor C = {(B.second + 1)%n, (A.first + n/2 - 1 + n)%n};
         if (factor_starts[C.first].find(C) != factor_starts[C.first].end()) {
           return {A, B, C};
         }
+      }
+    }
+  }
+  return {};
+}
+
+vector<Factor> has_half_turn_tiling(const string& P) {
+  int n = P.length();
+  auto mirror_factor_pairs = admissible_gapped_mirror_factor_pairs(P);
+  /*cout << "Printing mirrors";
+  for (auto& p: mirror_factor_pairs) {
+    cout << "(";
+    printFactor(p.first);
+    cout << ")";
+    cout << " (";
+    printFactor(p.second);
+    cout << ")\n";
+  }*/
+  vector<vector<Factor>> palindrome_factor_starts(n);
+  vector<vector<Factor>> palindrome_factor_ends(n);
+  for (auto& f: admissible_rotadrome_factors(P, 180)) {
+    palindrome_factor_starts[f.first].push_back(f);
+    palindrome_factor_ends[f.second].push_back(f);
+  }
+
+  Factor last_A;
+  Factor last_A_hat;
+  for (auto& p: mirror_factor_pairs)  {
+    Factor A = p.first;
+    Factor A_hat = p.second;
+
+    Factor dpi1 = make_pair((A.second+1)%n, (A_hat.first-1+n)%n);
+    Factor dpi2 = make_pair((A_hat.second+1)%n, (A.first-1+n)%n);
+    vector<Factor> dp1 = is_double_palindrome(dpi1, palindrome_factor_starts, palindrome_factor_ends, n);
+    vector<Factor> dp2 = is_double_palindrome(dpi2, palindrome_factor_starts, palindrome_factor_ends, n);
+
+    if (!dp1.empty() && !dp2.empty()) {
+      vector<Factor> result;
+      result.push_back(A);
+      result.insert(result.end(), dp1.begin(), dp1.end());
+      result.push_back(A_hat);
+      result.insert(result.end(), dp2.begin(), dp2.end());
+      return result;
+    }
+  }
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (j == i) {
+        continue;
+      }
+      Factor dpi1 = make_pair(i, (j-1+n)%n);
+      Factor dpi2 = make_pair(j%n, (i-1+n)%n);
+      vector<Factor> dp1 = is_double_palindrome(dpi1, palindrome_factor_starts, palindrome_factor_ends, n);
+      vector<Factor> dp2 = is_double_palindrome(dpi2, palindrome_factor_starts, palindrome_factor_ends, n);
+      if (!dp1.empty() && !dp2.empty()) {
+        vector<Factor> result;
+        result.push_back(last_A);
+        result.insert(result.end(), dp1.begin(), dp1.end());
+        result.push_back(last_A_hat);
+        result.insert(result.end(), dp2.begin(), dp2.end());
+        return result;
       }
     }
   }
@@ -105,45 +257,59 @@ void test_equal(const string& test_name, T& actual, T& expected) {
     }
 }
 
+/****************************************************
+* Code for tests 
+* This section contains:
+*   - Helper functions 
+*   - Functions that test functionality.
+*   - A main function which runs our test suit. 
+****************************************************/
 
-int main() {
-  // Test longest longest_match
-  string S1 = "abcdef";
-  string S2 = "abcfged";
+string create_square(int i) {
+  string square(4 * i, ' ');
+  fill(square.begin(), square.begin() + i, 'N');
+  fill(square.begin() + i, square.begin() + 2 * i, 'E');
+  fill(square.begin() + 2 * i, square.begin() + 3 * i, 'S');
+  fill(square.begin() + 3 * i, square.end(), 'W');
+  return square;
+}
 
-  assert(longest_match(S1, S2, 5) == 3);
-  assert(longest_match(S1, S2, 2) == 2);
+string create_long_rectangle(int i) {
+  string rectangle(2 * i + 2, ' ');
+  rectangle[0] = 'N';
+  fill(rectangle.begin() + 1, rectangle.begin() + i + 1, 'E');
+  rectangle[i + 1] = 'S';
+  fill(rectangle.begin() + i + 2, rectangle.end(), 'W');
+  return rectangle;
+}
 
-  // Test admissible_mirror_factors
-  string boundary = "EESSENESSESWWWSWSWNNWWNEEENWNN";
-  for (auto& p: admissible_mirror_factors(boundary)) {
-    cout << p.first << " " << p.second << "\n";
-  }
+string create_tall_rectangle(int i) {
+  string rectangle(2 * i + 2, ' ');
+  fill(rectangle.begin(), rectangle.begin() + i, 'N');
+  rectangle[i] = 'E';
+  fill(rectangle.begin() + i + 1, rectangle.begin() + 2 * i + 1, 'S');
+  rectangle[2 * i + 1] = 'W';
+  return rectangle;
+}
 
+void test__admissible_rotadrome_factors() {
+  vector<Factor> factors = admissible_rotadrome_factors("NESW", 180);
+  set<Factor> factorSet(factors.begin(), factors.end());
+  set<Factor> wantSet = {{0, 0}, {1, 1}, {2, 2}, {3, 3}};
+  assert(factorSet == wantSet);
+}
+
+void test__has_translation_tiling() {
   // Test has_translation_tiling
   for (int i = 1; i <= 5; ++i) {
-    string square(4 * i, ' ');
-    fill(square.begin(), square.begin() + i, 'N');
-    fill(square.begin() + i, square.begin() + 2 * i, 'E');
-    fill(square.begin() + 2 * i, square.begin() + 3 * i, 'S');
-    fill(square.begin() + 3 * i, square.end(), 'W');
-    assert(!has_translation_tiling(square).empty());
+    assert(!has_translation_tiling(create_square(i)).empty());
   }
 
   for (int i = 1; i <= 5; ++i) {
-    string rectangle(2 * i + 2, ' ');
-    rectangle[0] = 'N';
-    fill(rectangle.begin() + 1, rectangle.begin() + i + 1, 'E');
-    rectangle[i + 1] = 'S';
-    fill(rectangle.begin() + i + 2, rectangle.end(), 'W');
-    assert(!has_translation_tiling(rectangle).empty());
+    assert(!has_translation_tiling(create_long_rectangle(i)).empty());
 
     // Rectangle in other direction 
-    fill(rectangle.begin(), rectangle.begin() + i, 'N');
-    rectangle[i] = 'E';
-    fill(rectangle.begin() + i + 1, rectangle.begin() + 2 * i + 1, 'S');
-    rectangle[2 * i + 1] = 'W';
-    assert(!has_translation_tiling(rectangle).empty());
+    assert(!has_translation_tiling(create_tall_rectangle(i)).empty());
   }
 
   assert(!has_translation_tiling("NNESESSWNW").empty());
@@ -155,3 +321,66 @@ int main() {
   assert(has_translation_tiling("NENNESSSEESWWWNW").empty());
   assert(has_translation_tiling("WWWNNESENESS").empty());
 }
+
+void test__has_half_turn_tiling() {
+  /*for (int i = 1; i <= 5; ++i) {
+    assert(!has_half_turn_tiling(create_square(i)).empty());
+  }
+
+  for (int i = 1; i <= 5; ++i) {
+    assert(!has_half_turn_tiling(create_long_rectangle(i)).empty());
+    assert(!has_half_turn_tiling(create_tall_rectangle(i)).empty());
+  }
+
+  assert(!has_half_turn_tiling("NNESESSWWW").empty());
+  assert(!has_half_turn_tiling("NNNESESSWW").empty());*/
+
+  assert(!has_half_turn_tiling("NWNEENWNENESESESSWSWNWSW").empty());
+
+  assert(!has_half_turn_tiling("NENENESEESWSWNWSWW").empty());
+  assert(!has_half_turn_tiling("WWWNNESENESS").empty());
+
+  string B = "ENESEENES";
+  string reverseB = B;
+  reverse(reverseB.begin(), reverseB.end());
+  B += reverseB;
+
+  string C = "SSESSW";
+  string reverseC = C;
+  reverse(reverseC.begin(), reverseC.end());
+  C += reverseC;
+
+  string D = "WWWWWWWWWW";
+  
+  string E = "NWWNEE";
+  string reverseE = E;
+  reverse(reverseE.begin(), reverseE.end());
+  E += reverseE;
+
+  assert(!has_half_turn_tiling(B + C + D + E).empty());
+
+  assert(has_half_turn_tiling("NENNESSSEESWWWNW").empty());
+
+  assert(has_half_turn_tiling("WWWWWNNESEEENESS").empty());
+}
+
+int main() {
+  // Test longest longest_match
+  string S1 = "abcdef";
+  string S2 = "abcfged";
+
+  assert(longest_match(S1, S2, 5) == 3);
+  assert(longest_match(S1, S2, 2) == 2);
+
+  test__admissible_rotadrome_factors();
+
+  // Test admissible_mirror_factors
+  /*string boundary = "EESSENESSESWWWSWSWNNWWNEEENWNN";
+  for (auto& p: admissible_mirror_factors(boundary)) {
+    cout << p.first << " " << p.second << "\n";
+  }*/
+
+  test__has_translation_tiling();
+  test__has_half_turn_tiling();
+
+ }
