f0066fe2d4773a96336355b2ed10a10bc7a7bc6b
Michael Yang <michael777yang@gmail.com>
2025-01-29 16:32:51 -0500

    Implement half turn tiling and type 1 reflection tiling.

--- cpp/isohedral.cpp
+++ cpp/isohedral.cpp
@@ -32,6 +32,13 @@ unordered_map<char, char> CCW = {
   {'W', 'S'}
 };
 
+unordered_map<int, unordered_map<char, char>> REFL = {
+  {-45, {{'N', 'W'}, {'E', 'S'}, {'S', 'E'}, {'W', 'N'}}},
+  {0, {{'N', 'S'}, {'E', 'E'}, {'S', 'N'}, {'W', 'W'}}},
+  {45, {{'N', 'E'}, {'E', 'N'}, {'S', 'W'}, {'W', 'S'}}},
+  {90, {{'N', 'N'}, {'E', 'W'}, {'S', 'S'}, {'W', 'E'}}},
+};
+
 int longest_match(const string& S1, const string& S2, size_t ub) {
   int i = 0;
   while (i < min({S1.length(), S2.length(), ub}) && S1[i] == S2[i]) {
@@ -55,6 +62,26 @@ vector<Factor> is_double_palindrome(const Factor& F, const vector<vector<Factor>
     }
   }
   return {};
+}
+
+bool is_reflect_square_factor(const string& P, int i, int j, int theta) {
+  int n = P.length();
+  int l = j - i + 1 + n * (j < i);
+  if (l % 2 != 0) {
+    return false;
+  }
+  l /= 2;
+  string reflected;
+  /*cout << "i is: " << i << " l is: " << l << " sum is: " << i + l << "\n";
+  cout << "Substring is: " << P.substr(i + l) << "\n";*/
+
+  for (auto& c: P.substr(min(i + l, n))) {
+    reflected += REFL[theta][c];
+  }
+  for (auto& c: P) {
+    reflected += REFL[theta][c];
+  }
+  return (l == longest_match(P.substr(i) + P, reflected, l+1));
 }
 
 string inv_comp(const std::string& S) {
@@ -156,6 +183,25 @@ vector<Factor> admissible_rotadrome_factors(const string& P, int theta) {
   return factors;
 }
 
+vector<Factor> admissible_reflect_square_factors(const string& P) {
+  int n = P.length();
+
+  set<Factor> factors;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (j == i) {
+        continue;
+      }
+      for (int theta = -45; theta <= 90; theta += 45) {
+        if (is_reflect_square_factor(P, i, j, theta)) {
+          factors.insert({i, j});
+        }
+      }
+    }
+  }
+  return vector<Factor>(factors.begin(), factors.end());
+}
+
 vector<Factor> has_translation_tiling(const string& P) {
   int n = P.length();
   vector<Factor> factors = admissible_mirror_factors(P);
@@ -248,13 +294,71 @@ vector<Factor> has_half_turn_tiling(const string& P) {
   return {};
 }
 
-template <typename T>
-void test_equal(const string& test_name, T& actual, T& expected) {
-    if (actual == expected) {
-        cout << "[PASS] " << test_name << ": Actual = " << actual << ", Expected = " << expected << "\n";
-    } else {
-        cout << "[FAIL] " << test_name << ": Actual = " << actual << ", Expected = " << expected << "\n";
+vector<Factor> has_quarter_turn_tiling(const string& P) {
+  int n = P.length();
+  vector<Factor> palin_factors = admissible_rotadrome_factors(P, 180);
+  vector<Factor> ninety_factors = admissible_rotadrome_factors(P, 90);
+  vector<vector<Factor>> ninety_factor_starts(n);
+  for (auto& f: ninety_factors) {
+    ninety_factor_starts[f.first].push_back(f);
+  }
+  // Factorizations with non-empty palindrome factor
+  for (auto& C: palin_factors) {
+    int C_len = C.second - C.first + 1 + n * (C.second < C.first);
+    for (auto& A: ninety_factor_starts[(C.second + 1)%n]) {
+      int A_len = A.second - A.first + 1 + n * (A.second < A.first);
+      // One empty 90-drome factor
+      if (A_len + C_len == n) {
+        return {C, A};
+      }
+      // No empty 90-drome factors
+      for (auto& B: ninety_factor_starts[(A.second+1)%n]) {
+        int B_len = B.second - B.first + 1 + n * (B.second < B.first);
+        if (A_len + B_len + C_len == n) {
+          return {C, A, B};
+        }
+      }
     }
+  }
+  // Factorizations with empty palindrome factor
+  for (auto& A: ninety_factors) {
+    for (auto& B: ninety_factor_starts[(A.second + 1)%n]) {
+      int AB_len = A.second - A.first + 1 + n * (A.second < A.first) + B.second - B.first + 1 + n * (B.second < B.first);
+      if (AB_len == n) {
+        return {A, B};
+      }
+    }
+  }
+  return {};
+}
+
+vector<Factor> has_type_1_reflection_tiling(const string& P) {
+  int n = P.length();
+  vector<pair<Factor, Factor>> mirror_factor_pairs = admissible_gapped_mirror_factor_pairs(P);
+  vector<Factor> reflect_square_factors = admissible_reflect_square_factors(P);
+  vector<set<Factor>> reflect_square_factor_starts(n);
+  for (auto& f: reflect_square_factors){
+    reflect_square_factor_starts[f.first].insert(f);
+  }
+  for (auto& p: mirror_factor_pairs) {
+    Factor A = p.first;
+    Factor A_hat = p.second;
+    Factor rem1 = {(A.second+1)%n, (A_hat.first-1+n)%n};
+    Factor rem2 = {(A_hat.second+1)%n, (A.first-1+n)%n};
+    if (reflect_square_factor_starts[rem1.first].find(rem1) != reflect_square_factor_starts[rem1.first].end() && reflect_square_factor_starts[rem2.first].find(rem2) != reflect_square_factor_starts[rem2.first].end()) {
+      return {A, rem1, A_hat, rem2};
+    }
+  }
+  for (auto& f: reflect_square_factors) {
+    int f_len = f.second - f.first + 1 + n * (f.second < f.first);
+    for (auto& of: reflect_square_factor_starts[(f.second+1)%n]) {
+      int of_len = of.second - of.first + 1 + n * (of.second < of.first);
+      if (f_len + of_len == n) {
+        return {f, of};
+      }
+    }
+  }
+  return {};
 }
 
 /****************************************************
@@ -292,10 +396,42 @@ string create_tall_rectangle(int i) {
   return rectangle;
 }
 
+string create_2_by_i_rectangle(int i) {
+  string rectangle(2 * i + 4, ' ');
+  rectangle[0] = 'N';
+  rectangle[1] = 'N';
+  fill(rectangle.begin() + 2, rectangle.begin() + i + 2, 'E');
+  rectangle[i + 2] = 'S';
+  rectangle[i + 3] = 'S';
+  fill(rectangle.begin() + i + 4, rectangle.end(), 'W');
+  return rectangle;
+}
+
+string create_rectangle(int n, int m) {
+  string rectangle(2 * n + 2 * m, ' ');
+  fill(rectangle.begin(), rectangle.begin() + n, 'N');
+  fill(rectangle.begin() + n, rectangle.begin() + n + m, 'E');
+  fill(rectangle.begin() + n + m, rectangle.begin() + 2*n + m, 'S');
+  fill(rectangle.begin() + 2*n + m, rectangle.end(), 'W');
+  return rectangle;
+}
+
 void test__admissible_rotadrome_factors() {
   vector<Factor> factors = admissible_rotadrome_factors("NESW", 180);
   set<Factor> factorSet(factors.begin(), factors.end());
   set<Factor> wantSet = {{0, 0}, {1, 1}, {2, 2}, {3, 3}};
+  assert(factorSet == wantSet);
+}
+
+void test__admissible_reflect_square_factors() {
+  vector<Factor> factors = admissible_reflect_square_factors("NEESWW");
+  set<Factor> factorSet(factors.begin(), factors.end());
+  set<Factor> wantSet = {{0, 1}, {2, 3}, {3, 4}, {5, 0}, {1, 2}, {4, 5}};
+  assert(factorSet == wantSet);
+
+  factors = admissible_reflect_square_factors("NNEESSWW");
+  factorSet = set<Factor>(factors.begin(), factors.end());
+  wantSet = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 0}, {0, 3}, {2, 5}, {4, 7}, {6, 1}};
   assert(factorSet == wantSet);
 }
 
@@ -323,7 +459,7 @@ void test__has_translation_tiling() {
 }
 
 void test__has_half_turn_tiling() {
-  /*for (int i = 1; i <= 5; ++i) {
+  for (int i = 1; i <= 5; ++i) {
     assert(!has_half_turn_tiling(create_square(i)).empty());
   }
 
@@ -333,7 +469,7 @@ void test__has_half_turn_tiling() {
   }
 
   assert(!has_half_turn_tiling("NNESESSWWW").empty());
-  assert(!has_half_turn_tiling("NNNESESSWW").empty());*/
+  assert(!has_half_turn_tiling("NNNESESSWW").empty());
 
   assert(!has_half_turn_tiling("NWNEENWNENESESESSWSWNWSW").empty());
 
@@ -364,6 +500,37 @@ void test__has_half_turn_tiling() {
   assert(has_half_turn_tiling("WWWWWNNESEEENESS").empty());
 }
 
+void test__has_quarter_turn_tiling(){
+  for (int i = 1; i <= 5; ++i) {
+    assert(!has_quarter_turn_tiling(create_square(i)).empty());
+  }
+
+  assert(!has_quarter_turn_tiling(create_long_rectangle(2)).empty());
+  for (int i = 3; i <= 5; ++i) {
+    assert(has_quarter_turn_tiling(create_long_rectangle(i)).empty());
+  }
+
+  assert(!has_quarter_turn_tiling("NNNESESSWW").empty());
+  assert(has_quarter_turn_tiling("NNENESESSWNWSW").empty());
+}
+
+void test__has_type_1_reflection_tiling() {
+  for (int i = 1; i <= 5; ++i) {
+    assert(!has_type_1_reflection_tiling(create_square(i)).empty());
+  }
+
+  for (int i = 1; i <= 5; ++i) {
+    assert(!has_type_1_reflection_tiling(create_2_by_i_rectangle(i)).empty());
+  }
+
+  for (int i = 4; i <= 7; ++i) {
+    assert(!has_type_1_reflection_tiling(create_rectangle(3, i)).empty());
+  }
+
+  assert(has_type_1_reflection_tiling("NWNENENESESESWSWNWSW").empty());
+  assert(!has_type_1_reflection_tiling("NNWNENNNENWNEEEENESEEESESESESWSWSWWWWNWSWWWW").empty());
+}
+
 int main() {
   // Test longest longest_match
   string S1 = "abcdef";
@@ -373,6 +540,7 @@ int main() {
   assert(longest_match(S1, S2, 2) == 2);
 
   test__admissible_rotadrome_factors();
+  test__admissible_reflect_square_factors();
 
   // Test admissible_mirror_factors
   /*string boundary = "EESSENESSESWWWSWSWNNWWNEEENWNN";
@@ -382,5 +550,7 @@ int main() {
 
   test__has_translation_tiling();
   test__has_half_turn_tiling();
+  test__has_quarter_turn_tiling();
+  test__has_type_1_reflection_tiling();
 
  }
